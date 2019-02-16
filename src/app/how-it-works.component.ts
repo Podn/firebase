2ff5280b9be49d478ab0591334f34a8b70d23745
dgriff03 <daniel.griffin@tufts.edu>
2019-02-15 21:55:18 -0800

    how it works page

--- src/app/how-it-works.component.ts
+++ src/app/how-it-works.component.ts
@@ -5,4 +5,74 @@ import { Component } from '@angular/core';
   templateUrl: './how-it-works.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class HowItWorksComponent {};
+export class HowItWorksComponent {
+    private currentDisplayed: string = '';
+
+    show(name: string) {
+        this.currentDisplayed = name;
+    }
+
+    get sectionList() {
+        if (this.currentDisplayed === 'basic') {
+            return [
+                {
+                    title: 'Making your raw audio an episode',
+                    items: [
+                        'Submit Raw Audio and any episode specific audio',
+                        'We remove the Ahs and Ums and balance volumes',
+                        'Recieve a polished podcast episide',
+                    ],
+                },
+            ];
+        }
+        if (this.currentDisplayed === 'premium') {
+            return [
+                {
+                    title: 'Making your raw audio an episode',
+                    items: [
+                        'Submit Raw Audio and any episode specific audio',
+                        'We remove the Ahs and Ums and balance volumes',
+                        'Recieve as transcript',
+                        'Apply comments and edits to the transcript',
+                        'Recieve a polished podcast episide',
+                    ],
+                },
+            ];
+
+            
+        }
+        if (this.currentDisplayed === 'enterprise') {
+            return [
+                {
+                    title: 'Getting the podcast online',
+                    items: [
+                        'Set up podcast website',
+                        'Find intro / outro music',
+                        'Find podcast art',
+                        'Submit podcast to all client services',
+                    ],
+                },
+                {
+                    title: 'Making your raw audio an episode',
+                    items: [
+                        'Submit Raw Audio and any episode specific audio',
+                        'We remove the Ahs and Ums and balance volumes',
+                        'Recieve as transcript',
+                        'Apply comments and edits to the transcript',
+                        'Recieve a polished podcast episide',
+                    ],
+                },
+                {
+                    title: 'Getting your episode online',
+                    items: [
+                        'Write up per-episode description',
+                        'Signoff of on final episode, title and description',
+                        'Post podcast on hosting',
+                        'Recieve podcast statistics',
+                    ],
+                },
+            ];    
+        }
+        return [];
+    }
+};
